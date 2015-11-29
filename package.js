41d984231d53aa428b8660d6a1a21149839da747
Donald Averill <donaldaverill@gmail.com>
2015-11-29 09:07:40 -0800

    update package.js to add git information.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'fourquet:inspinia-text-field',
   version: '0.0.1',
   summary: 'Blaze text input component to use with the Inspinia Bootstrap Admin Theme',
-  git: '',
+  git: 'https://github.com/fourquet/meteor-package-inspinia-text-field',
   documentation: 'README.md',
   license: 'LICENSE',
 });
@@ -16,18 +16,10 @@ const clientFiles = [
   'textField.html',
   'textField.js',
 ];
-const serverFiles = [
-
-];
-const files = [
-
-];
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1');
   api.use(packages);
   api.imply(packages);
   api.addFiles(clientFiles, 'client');
-  api.addFiles(serverFiles, 'server');
-  api.addFiles(files);
 });
